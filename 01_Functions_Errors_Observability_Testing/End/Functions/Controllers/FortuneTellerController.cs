cbbaf8720cb2035ff23ff946ec0d27fe9d5afab3
Stas Lebedenko <>
2021-12-05 22:50:34 +0200

    Step 1 End initial commit

---
+++ 01_Functions_Errors_Observability_Testing/End/Functions/Controllers/FortuneTellerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public class FortuneTellerController
+    {
+        private readonly ILogger log;
+
+        public FortuneTellerController(ILogger<FortuneTellerController> log)
+        {
+            this.log = log;
+        }
+
+        [FunctionName("AskZoltar")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route =  "api/AskZoltar/{name}")]
+            HttpRequest req,
+            string name)
+        {
+            var rate = RatePrediction();
+
+            var prediction = $"Zoltar speaks! {name}, your rate will be '{rate}'.";
+
+            log.LogInformation($"Prediction is done => {prediction}");
+
+            return (ActionResult)new OkObjectResult(prediction);
+        }
+
+        private static int RatePrediction()
+        {
+            var random = new Random();
+            return random.Next(40, 90);
+        }
+    }
+}
---
+++ 01_Functions_Errors_Observability_Testing/End/Functions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Functions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging(options =>
+            {
+                options.AddFilter("Functions", LogLevel.Information);
+            });
+        }
+    }
+}
