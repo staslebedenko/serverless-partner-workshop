7083a66c1a9cc804f637700cc80ebebc866439a7
Stas Lebedenko <>
2021-12-05 20:50:04 +0200

    loader.io activation

---
+++ 01_Functions_Errors_Observability_Testing/Start/Functions/Controllers/LoaderController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Functions.Controllers
+{
+    public static class LoaderController
+    {
+        [FunctionName("LoaderActivation")]
+        public static IActionResult LoaderActivation(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "loaderio-")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Loader.io validation triggered.");
+            return (ActionResult)new OkObjectResult($"loaderio-");
+        }
+    }
+}
