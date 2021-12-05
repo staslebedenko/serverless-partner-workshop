dde9a847f33a1843d17f7a4eac4d08e81150aee4
Stas Lebedenko <>
2021-12-05 21:51:25 +0200

    Salary rate prediction controller

---
+++ 01_Functions_Errors_Observability_Testing/Start/Functions/Controllers/FortuneTellerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public class FortuneTellerController
+    {
+        [FunctionName("AskZoltar")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route =  "api/AskZoltar/{name}")]
+            HttpRequest req,
+            string name,
+            ILogger log)
+        {
+            var rate = RatePrediction();
+
+            var prediction = $"Zoltar speaks! {name}, your rate will be '{rate}'.";
+
+            return (ActionResult)new OkObjectResult(prediction);
+        }
+
+        private static int RatePrediction()
+        {
+            var random = new Random();
+            return random.Next(40, 90);
+        }
+    }
+}
--- 01_Functions_Errors_Observability_Testing/Start/Functions/Controllers/LoaderController.cs
+++ 01_Functions_Errors_Observability_Testing/Start/Functions/Controllers/LoaderController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Functions.Controllers
+namespace Functions
 {
     public static class LoaderController
     {
