d16cebbba368f373c2fa2266c42ef9545b7c9d3a
Stas Lebedenko <>
2021-12-09 20:21:43 +0200

    Step 3 initial commit

---
+++ 03_Durable_functions/End/Data/Configuration/FunctionContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Data
+{
+    public class FunctionContextFactory : IDesignTimeDbContextFactory<FunctionDbContext>
+    {
+        public FunctionDbContext CreateDbContext(string[] args)
+        {
+            string sqlString = Environment.GetEnvironmentVariable("PartnerSqlString");
+            string password = Environment.GetEnvironmentVariable("PartnerSqlPassword");
+            string connectionString = SqlConnectionBuilder.GetConnectionString(sqlString, password);
+
+            var optionsBuilder = new DbContextOptionsBuilder<FunctionDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new FunctionDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ 03_Durable_functions/End/Functions/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Data;
+using Functions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging(options =>
+            {
+                options.AddFilter("Functions", LogLevel.Information);
+            });
+            string sqlString = Environment.GetEnvironmentVariable("PartnerSqlString");
+            string password = Environment.GetEnvironmentVariable("PartnerSqlPassword");
+            string connectionString = SqlConnectionBuilder.GetConnectionString(sqlString, password);
+
+            //builder.Services.AddDbContextPool<FunctionDbContext>(
+            builder.Services.AddDbContext<FunctionDbContext>(
+               options =>
+               {
+                   if (!string.IsNullOrEmpty(connectionString))
+                   {
+                       options.UseSqlServer(connectionString, providerOptions => providerOptions.EnableRetryOnFailure());
+                   }
+               });
+
+            FunctionDbContext.ExecuteMigrations(connectionString);
+
+            string cosmosUrl = Environment.GetEnvironmentVariable("PartnerCosmosUrl");
+            string cosmosKey = Environment.GetEnvironmentVariable("PartnerCosmosKey");
+            string cosmosDb = Environment.GetEnvironmentVariable("PartnerCosmosDatabase");
+
+            builder.Services.AddDbContext<CosmosDbContext>(options =>
+            {
+                options.UseCosmos(
+                    cosmosUrl,
+                    cosmosKey,
+                    cosmosDb);
+            });
+
+            CosmosDbContext.ExecuteMigrations(cosmosUrl, cosmosKey, cosmosDb);
+        }
+    }
+}
